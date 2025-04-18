2fc4888caf96e46a77ef805b78fdea451005787c
pcfeng <123@qq.com>
2025-04-18 19:06:15 +0800

    [+] init

---
+++ README.md
@@ -0,0 +1,1 @@
+#  peak-central-platform
---
+++ peak-im/src/main/java/com/peak/Main.java
@@ -0,0 +1,7 @@
+package com.peak;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
